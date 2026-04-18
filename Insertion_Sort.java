237431d84a0b3b7674333cca06b9a5cc5e4cb420
Arpitsoniofficial0214 <154724745+Arpitsoniofficial0214@users.noreply.github.com>
2026-04-18 10:57:06 +0530

    Add files via upload

---
+++ Insertion_Sort.java
@@ -0,0 +1,25 @@
+package Monu_Bhaiya_DSA;
+
+public class Insertion_Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr [] = {2,13,-4,5,7};
+		Sort(arr);
+		 for(int i = 0; i<arr.length; i++) {
+			 System.out.print(arr[i] + " ");
+		 }
+}
+	
+	public static void Sort(int arr[]) {
+		for(int i = 1; i < arr.length; i++) {
+			int picked = arr[i];
+			int j = i - 1;
+			while(j >= 0 && arr[j] > picked) {
+				arr[j+1] = arr[j];
+				j--;
+			}
+			arr[j+1] = picked;
+		}
+	}
+}
